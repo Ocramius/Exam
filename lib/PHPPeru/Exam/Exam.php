7f269dadf298784ea9414db2c6b9c4646053bb5f
Luis Cordova <cordoval@gmail.com>
2012-03-09 12:50:12 -0500

    first empty :D passing spec

--- lib/PHPPeru/Exam/Exam.php
+++ lib/PHPPeru/Exam/Exam.php
@@ -4,27 +4,21 @@
  * This file is part of the PHPPeru packages.
  *
  * (c) Luis Cordova <cordoval@gmail.com>
- * (c) Marco Pivetta <ocramius@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace PHPPeru\Exam\Spec;
-
-use PHPPeru\Exam\Exam;
+namespace PHPPeru\Exam;
 
 /**
- * ExamSpec.php: Short description.
+ * Exam.php: Short description.
  *
  * Two line explanation.
  *
  * @author Luis Cordova <cordoval@gmail.com>
  */
-class DescribeExam extends \PHPSpec\Context
+class Exam
 {
-    public function itShouldTestSomething()
-    {
 
-    }
 }
