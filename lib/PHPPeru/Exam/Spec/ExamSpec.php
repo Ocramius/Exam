7ffa6cf2d53a9ddff9aff3f86864cd4907268f6f
Luis Cordova <cordoval@gmail.com>
2012-03-09 12:07:59 -0500

    added first spec wip

---
+++ lib/PHPPeru/Exam/Spec/ExamSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the PHPPeru packages.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ * (c) Marco Pivetta <ocramius@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPPeru\ProjectFolder\SubFolder;
+
+use PHPPeru\ProjectFolder\SubFolder\ClassName;
+
+/**
+ * ExamSpec.php: Short description.
+ *
+ * Two line explanation.
+ *
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class DescribeExam
+{
+
+}
